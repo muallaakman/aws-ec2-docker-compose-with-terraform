95f7c600d1aaf5941f99bee00a628b62bcd0706d
Mualla Akman <akmanmualla28@gmail.com>
2024-04-17 21:59:01 +0300

    tf-files added

---
+++ README.md
@@ -0,0 +1,11 @@
+# aws-docker-with-terraform
+
+
+you can create with this code
+
+- create a VPC
+- create single subnet
+- create NSG 
+- ec2 ubuntu with docker and docker-compose
+- copy docker-compose.yml with file provisioner
+- docker compose up with remote_exec provsioner
---
+++ provider.tf
@@ -0,0 +1,31 @@
+terraform {
+
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.25.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+provider "tls" {
+}
+
+provider "random" {
+}
